d147162058f0ebec9d1d0539d6af258b7df4f630
AdityaArun1435 <70952042+AdityaArun1435@users.noreply.github.com>
2026-06-13 13:21:33 +0530

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    'https://exmhuwausgvyhkmuwxft.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4bWh1d2F1c2d2eWhrbXV3eGZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NTU0NjEsImV4cCI6MjA5NDUzMTQ2MX0.NikIhBue_eoFowS9LbZTJ-zvQtjASoO7giam8JP8v7o'
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
